3e4ef9cb90dcbc5f5511a4ef6b4481ed01a6ec90
Yugandhar Reddy <Yugandhar.Reddy@halliburton.com>
2023-07-24 16:37:55 +0530

    Add project files.

---
+++ TestDocumentFormat/Program.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace TestDocumentFormat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+            try
+            {
+                using (WordprocessingDocument wordDocument =
+                    WordprocessingDocument.Create("TestFile.doc", WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                    // Create the document structure and add some text.
+                    mainPart.Document = new Document();
+                    Body body = mainPart.Document.AppendChild(new Body());
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+                    run.AppendChild(new Text("Create text in body - CreateWordprocessingDocument"));
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(Directory.GetFiles(Directory.GetCurrentDirectory()));
+            Thread.Sleep(300000);
+        }
+    }
+}
---
+++ TestReportAPI/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Mvc;
+using Document = DocumentFormat.OpenXml.Wordprocessing.Document;
+using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
+
+namespace TestReportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet(nameof(GetReport))]
+        public ActionResult GetReport()
+        {
+            var data = GetFile();
+            return File(data, "application/msword");
+        }
+
+        private byte[] GetFile()
+        {
+            using (WordprocessingDocument wordDocument =
+            WordprocessingDocument.Create(@"Test.doc", WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                run.AppendChild(new Text("Create text in body - CreateWordprocessingDocument"));
+            }
+            return System.IO.File.ReadAllBytes("Test.doc");
+        }
+    }
+}
